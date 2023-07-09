410bba1dabd8da0dcd345a9708d69f289e027ad6
Nguyen Quang Dang <dangnguyenquangit@gmail.com>
2023-07-09 19:01:52 +0700

    QD update app.js && doc for gateway

--- 1.Code/1.Gateway/app.js
+++ 1.Code/1.Gateway/app.js
@@ -1,13 +1,34 @@
+const { SerialPort } = require('serialport')
 const { Kafka, Partitioners } = require('kafkajs');
-
 const kafkaServer = '173.249.14.91:29092';
-const topic = 'mes_StartIOTRequest';
-const jsonData = {
-    "machineCode": "BI0001",
-    "currentMillisecond": 1686082323858,
-   }; // Đối tượng JSON
 
-async function produceMessage() {
+const topicCount = 'mes_CounterIOTRequest';
+const topicAlive = 'mes_AliveIOTRequest';
+const topicBegin = 'mes_StartIOTRequest';
+const topicStop = 'mes_StopIOTRequest';
+
+const portName = 'COM3';
+const hexArray = ['0x03', '0x03', '0x01', '0xAA']; // sẽ thay đổi
+
+let preIndexs = 0;
+let indexs = 0;
+let lastedBtn = '0';
+let status;
+let receivedData = []; 
+let expectedDataCount = 16;
+let intervalMillisecond = 60000;
+let lastMilliseconds = new Date().getTime();
+
+// Tạo đối tượng SerialPort với cổng và các tùy chọn
+const port = new SerialPort({
+  path: portName,
+  baudRate: 115200, // Tốc độ baud
+  dataBits: 8, // Số bit dữ liệu
+  parity: 'none', // Kiểm tra chẵn/lẻ
+  stopBits: 1, // Số bit dừng
+});
+
+async function produceMessage(topic, jsonData) {
   const kafka = new Kafka({
     clientId: 'TEST',
     brokers: [kafkaServer],
@@ -21,6 +42,7 @@ async function produceMessage() {
 
   const jsonString = JSON.stringify(jsonData);
   const buffer = Buffer.from(jsonString, 'utf8');
+  lastMilliseconds = jsonData.currentMillisecond;
 
   await producer.send({
     topic: topic,
@@ -30,6 +52,122 @@ async function produceMessage() {
   await producer.disconnect();
 }
 
-produceMessage().catch((error) => {
-  console.error('Error producing message:', error);
+// Lắng nghe sự kiện 'open' khi cổng serial đã mở
+port.on('open', () => {
+
+  setInterval(() => {
+    port.write(Buffer.from(hexArray));
+  }, 5000);
+  
+  console.log('Cổng serial đã mở.');
+  port.on('data', (data) => {
+    receivedData = receivedData.concat([...data]); // Gộp dữ liệu vào mảng receivedData
+    
+    if (receivedData.length === expectedDataCount) {
+      const combinedValue = (receivedData[9] << 8) | receivedData[10]; 
+      let machineCode = receivedData[0].toString(16) + receivedData[1].toString(16);
+      let currentMillisecond = new Date().getTime();
+
+      if (combinedValue < preIndexs){
+        indexs += combinedValue + 65535 - preIndexs + 1;
+      } else {
+        indexs += combinedValue - preIndexs;
+      }
+      
+      preIndexs = combinedValue;
+      
+      if (receivedData[12].toString(16) != lastedBtn){
+        lastedBtn = receivedData[12].toString(16);
+        if (receivedData[12].toString(16) == '1'){
+          indexs = 0;
+          preIndexs = combinedValue;
+          jsonDataBegin = {
+            "machineCode": machineCode,     
+            "currentMillisecond": currentMillisecond,     
+            "intervalMillisecond": intervalMillisecond, 
+          }
+
+          produceMessage(topicBegin, jsonDataBegin)
+          .then(() => {
+            console.log('Message begin successfully.');
+          })
+          .catch((error) => {
+            console.error('Error producing message:', error);
+          });
+        } else if (receivedData[12].toString(16) == '0'){
+          jsonDataStop = {
+            "machineCode": machineCode,     
+            "currentMillisecond": currentMillisecond,     
+            "lastMillisecond": lastMilliseconds,     
+            "lastIndex": indexs, 
+          }
+          produceMessage(topicStop, jsonDataStop)
+          .then(() => {
+            console.log('Message stop successfully.');
+          })
+          .catch((error) => {
+            console.error('Error producing message:', error);
+          });
+        }
+      }
+      
+      if (receivedData[4].toString(16) == '1'){
+        status = 0;
+      } else if (receivedData[6].toString(16) == '1'){
+        status = 1;
+      } else if (receivedData[8].toString(16) == '1'){
+        status = 2;
+      }
+
+      console.log('Mảng byte nhận được (hex):', combinedValue);
+      jsonDataCount = {
+        "machineCode": machineCode,
+        "currentMillisecond": currentMillisecond,
+        "lastMillisecond": lastMilliseconds,
+        "index": indexs, 
+      }
+      
+      jsonDataAlive = {
+        "machineCode": machineCode,
+        "currentMillisecond": currentMillisecond,
+        "status": status,   
+      }
+      
+      receivedData = []; 
+    }
+  });
+});
+
+// Count
+setInterval(() => {
+  produceMessage(topicCount, jsonDataCount)
+    .then(() => {
+      console.log('Message count successfully.');
+    })
+    .catch((error) => {
+      console.error('Error producing message:', error);
+    });
+}, 60000);
+
+// Alive
+setInterval(() => {
+  produceMessage(topicAlive, jsonDataAlive)
+    .then(() => {
+      console.log('Message alive successfully.');
+    })
+    .catch((error) => {
+      console.error('Error producing message:', error);
+    });
+}, 60000);
+
+// Đóng cổng Serial Port khi kết thúc chương trình
+process.on('SIGINT', () => {
+  port.close((error) => {
+    if (error) {
+      console.error('Lỗi khi đóng cổng Serial Port:', error);
+    } else {
+      console.log('Cổng Serial Port đã đóng.');
+      process.exit();
+    }
+  });
 });
