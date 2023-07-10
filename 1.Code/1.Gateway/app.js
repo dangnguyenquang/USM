3bbc9060aada34165c18749ce0762e0d17e4c11e
Nguyen Quang Dang <dangnguyenquangit@gmail.com>
2023-07-10 18:03:43 +0700

    QD update code && doc for GateWay lan 2

--- 1.Code/1.Gateway/app.js
+++ 1.Code/1.Gateway/app.js
@@ -8,16 +8,43 @@ const topicBegin = 'mes_StartIOTRequest';
 const topicStop = 'mes_StopIOTRequest';
 
 const portName = 'COM3';
-let expectedDataCount = 17;
-
-let preIndexs = 0;
-const hexArray = ['0x05', '0x05', '0x01', '0xAA']; // sẽ thay đổi
-let indexs = 0;
-let lastedBtn = '0';
-let status;
-let receivedData = []; 
 let intervalMillisecond = 60000;
+let expectedDataCount = 16;
 let lastMilliseconds = new Date().getTime();
+let receivedData = [];
+
+let device = [
+  {
+    hexArray: [0x03, 0x03, 0x01, 0xAA],
+    preIndexs: 0,
+    indexs: 0,
+    lastedBtn: '0',
+    status: 0,
+    jsonDataAlive: {},
+    jsonDataCount: {},
+    alive: false
+  },
+  {
+    hexArray: [0x04, 0x04, 0x01, 0xAA],
+    preIndexs: 0,
+    indexs: 0,
+    lastedBtn: '0',
+    status: 0,
+    jsonDataAlive: {},
+    jsonDataCount: {},
+    alive: false
+  },
+  {
+    hexArray: [0x04, 0x04, 0x01, 0xAA],
+    preIndexs: 0,
+    indexs: 0,
+    lastedBtn: '0',
+    status: 0,
+    jsonDataAlive: {},
+    jsonDataCount: {},
+    alive: false
+  },
+];
 
 // Tạo đối tượng SerialPort với cổng và các tùy chọn
 const port = new SerialPort({
@@ -53,111 +80,125 @@ async function produceMessage(topic, jsonData) {
 }
 
 // Lắng nghe sự kiện 'open' khi cổng serial đã mở
-port.on('open', () => {
 
-  setInterval(() => {
-    port.write(Buffer.from(hexArray));
-  }, 5000);
-  
-  console.log('Cổng serial đã mở.');
-  port.on('data', (data) => {
-    receivedData = receivedData.concat([...data]); // Gộp dữ liệu vào mảng receivedData
-    
+function sendAndReceiveData(i) {
+  if (i >= device.length) {
+    i = 0;
+  }
+
+  console.log('Thiết bị thứ: ', i);
+  const currentDevice = device[i]; // Lấy đối tượng hiện tại từ mảng device
+  const arraySend = currentDevice.hexArray;
+  port.write(Buffer.from(arraySend));
+
+  const dataListener = (data) => {
+    receivedData = receivedData.concat([...data]);
+
     if (receivedData.length === expectedDataCount) {
-      const combinedValue = (receivedData[10] << 8) | receivedData[11]; 
+      currentDevice.alive = true;
+      const combinedValue = (receivedData[9] << 8) | receivedData[10];
       let machineCode = receivedData[0].toString(16) + receivedData[1].toString(16);
       let currentMillisecond = new Date().getTime();
 
-      if (combinedValue < preIndexs){
-        indexs += combinedValue + 65535 - preIndexs + 1;
+      if (combinedValue < currentDevice.preIndexs) {
+        currentDevice.indexs += combinedValue + 65535 - currentDevice.preIndexs + 1;
       } else {
-        indexs += combinedValue - preIndexs;
+        currentDevice.indexs += combinedValue - currentDevice.preIndexs;
       }
-      
-      preIndexs = combinedValue;
-      
-      if (receivedData[13].toString(16) != lastedBtn){
-        lastedBtn = receivedData[13].toString(16);
-        if (receivedData[13].toString(16) == '1'){
-          indexs = 0;
-          preIndexs = combinedValue;
+
+      currentDevice.preIndexs = combinedValue;
+
+      if (receivedData[12].toString(16) != currentDevice.lastedBtn) {
+        currentDevice.lastedBtn = receivedData[12].toString(16);
+        if (receivedData[12].toString(16) == '1') {
+          currentDevice.indexs = 0;
+          currentDevice.preIndexs = combinedValue;
           jsonDataBegin = {
-            "machineCode": machineCode,     
-            "currentMillisecond": currentMillisecond,     
-            "intervalMillisecond": intervalMillisecond, 
+            "machineCode": machineCode,
+            "currentMillisecond": currentMillisecond,
+            "intervalMillisecond": intervalMillisecond,
           }
 
           produceMessage(topicBegin, jsonDataBegin)
-          .then(() => {
-            console.log('Message begin successfully.');
-          })
-          .catch((error) => {
-            console.error('Error producing message:', error);
-          });
-        } else if (receivedData[13].toString(16) == '0'){
+            .then(() => {
+              console.log('Message begin successfully.');
+            })
+            .catch((error) => {
+              console.error('Error producing message:', error);
+            });
+        } else if (receivedData[12].toString(16) == '0') {
           jsonDataStop = {
-            "machineCode": machineCode,     
-            "currentMillisecond": currentMillisecond,     
-            "lastMillisecond": lastMilliseconds,     
-            "lastIndex": indexs, 
+            "machineCode": machineCode,
+            "currentMillisecond": currentMillisecond,
+            "lastMillisecond": lastMilliseconds,
+            "lastIndex": currentDevice.indexs,
           }
           produceMessage(topicStop, jsonDataStop)
-          .then(() => {
-            console.log('Message stop successfully.');
-          })
-          .catch((error) => {
-            console.error('Error producing message:', error);
-          });
+            .then(() => {
+              console.log('Message stop successfully.');
+            })
+            .catch((error) => {
+              console.error('Error producing message:', error);
+            });
         }
       }
-      
-      if (receivedData[4].toString(16) == '1'){
-        status = 0;
-      } else if (receivedData[6].toString(16) == '1'){
-        status = 1;
-      } else if (receivedData[8].toString(16) == '1'){
-        status = 2;
+
+      if (receivedData[4].toString(16) == '1') {
+        currentDevice.status = 0;
+      } else if (receivedData[6].toString(16) == '1') {
+        currentDevice.status = 1;
+      } else if (receivedData[8].toString(16) == '1') {
+        currentDevice.status = 2;
       }
 
-      console.log('Mảng byte nhận được (hex):', combinedValue);
-      jsonDataCount = {
+      console.log('Số lượng sản phẩm ghi nhận được: ', currentDevice.indexs);
+      currentDevice.jsonDataCount = {
         "machineCode": machineCode,
         "currentMillisecond": currentMillisecond,
         "lastMillisecond": lastMilliseconds,
-        "index": indexs, 
+        "index": currentDevice.indexs,
       }
-      
-      jsonDataAlive = {
+
+      currentDevice.jsonDataAlive = {
         "machineCode": machineCode,
         "currentMillisecond": currentMillisecond,
-        "status": status,   
+        "status": currentDevice.status,
       }
-      
-      receivedData = []; 
+
+      receivedData = [];
     }
-  });
-});
+  };
 
-// Count
-setInterval(() => {
-  produceMessage(topicCount, jsonDataCount)
-    .then(() => {
-      console.log('Message count successfully.');
-    })
-    .catch((error) => {
-      console.error('Error producing message:', error);
-    });
-}, 60000);
+  setTimeout(() => {
+    port.off('data', dataListener);
+    sendAndReceiveData(i + 1); // Gọi đệ quy để chạy chương trình cho đối tượng tiếp theo
+  }, 5000);
+  port.on('data', dataListener);
+}
 
-// Alive
+sendAndReceiveData(0);
+
+// Count && Alive
 setInterval(() => {
-  produceMessage(topicAlive, jsonDataAlive)
-    .then(() => {
-      console.log('Message alive successfully.');
-    })
-    .catch((error) => {
-      console.error('Error producing message:', error);
-    });
+  for (let k = 0; k < device.length; k++) {
+    if (device[k].alive == true) {
+      device[k].alive = false;
+      produceMessage(topicCount, device[k].jsonDataCount)
+        .then(() => {
+          console.log('Message count successfully.');
+        })
+        .catch((error) => {
+          console.error('Error producing message:', error);
+        });
+      produceMessage(topicAlive, device[k].jsonDataAlive)
+        .then(() => {
+          console.log('Message alive successfully.');
+        })
+        .catch((error) => {
+          console.error('Error producing message:', error);
+        });
+    }
+  }
 }, 60000);
 
 // Đóng cổng Serial Port khi kết thúc chương trình
--- 1.Code/1.Gateway/tempCodeRunnerFile.js
+++ 1.Code/1.Gateway/tempCodeRunnerFile.js
@@ -1,1 +1,18 @@
-intervalMilisecond
+  // {
+  //   hexArray: [0x04, 0x04, 0x01, 0xAA],
+  //   preIndexs: 0,
+  //   indexs: 0,
+  //   lastedBtn: '0',
+  //   status: 0,
+  //   jsonDataAlive: {},
+  //   jsonDataCount: {},
+  // },
+  // {
+  //   hexArray: [0x04, 0x04, 0x01, 0xAA],
+  //   preIndexs: 0,
+  //   indexs: 0,
+  //   lastedBtn: '0',
+  //   status: 0,
+  //   jsonDataAlive: {},
+  //   jsonDataCount: {},
+  // },
